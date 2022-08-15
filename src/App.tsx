46ef54df2d91afccc90c75f674f43cc067454077
haako <haako@computas.com>
2022-08-16 00:17:10 +0200

    Fullfør appen

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,113 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { DeleteIcon } from './components/DeleteIcon';
+import firestoreService, {
+  addThing,
+  deleteThing,
+  onThingsUpdate,
+  Thing,
+  ThingWithId,
+} from './services/firestore';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [things, setThings] = useState<ThingWithId[]>([]);
+  const [text, setText] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const unsubscribe = onThingsUpdate(
+      (thingsSnap) => {
+        const things = thingsSnap.docs
+          .map((thing) => {
+            if (!thing.exists()) return null;
+            return { ...(thing.data() as Thing), id: thing.id };
+          })
+          .filter((thing) => !!thing) as ThingWithId[];
+
+        setError('');
+        setThings(things);
+        setLoading(false);
+      },
+      () => {
+        setError('Could not get things');
+      },
+    );
+
+    return unsubscribe;
+  }, []);
+
+  const newThing = () => {
+    if (!text) return;
+    addThing(text);
+    setText('');
+  };
+
+  if (error) return <p>{error}</p>;
 
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div className='App card bg-neutral text-neutral-content w-96 m-auto'>
+      <div className='card-body flex flex-col items-start gap-2'>
+        <label className='input-group justify-center'>
+          <input
+            type='text'
+            id='thingInput'
+            placeholder='Legg til en ny ting'
+            value={text}
+            onChange={(text) => setText(text.target.value)}
+            className='input input-bordered w-full'
+          />
+          <button disabled={!text} onClick={newThing} className='btn btn-primary' type='submit'>
+            Add
+          </button>
+        </label>
+        <div className='divider'></div>
+        {!loading || <button className='btn btn-square loading'></button>}
+        {loading || things.map((thing) => <ThingItem key={thing.id} thing={thing} />)}
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
 
 export default App;
+
+interface ThingItemProps {
+  thing: ThingWithId;
+}
+
+function ThingItem({ thing }: ThingItemProps): JSX.Element {
+  const [newThing, setNewThing] = useState(thing.content);
+
+  const removeThing = async (thingId: string) => {
+    await deleteThing(thingId);
+  };
+
+  const editThing = async (thingId: string) => {
+    if (!newThing) return;
+    await firestoreService.editThing(thingId, newThing);
+  };
+
+  return (
+    <div key={thing.id} className='alert shadow-lg break-words'>
+      <input
+        className={`input input-bordered w-full max-w-xs ${newThing ? '' : 'border-red-500'}`}
+        type='text'
+        value={newThing}
+        onChange={(event) => {
+          setNewThing(event.target.value);
+        }}
+      ></input>
+      <button
+        disabled={!newThing || newThing === thing.content}
+        onClick={() => editThing(thing.id)}
+        className='btn btn-sm btn-success'
+      >
+        Edit
+      </button>
+      <button onClick={() => removeThing(thing.id)} className='btn btn-square btn-sm btn-error'>
+        <DeleteIcon />
+      </button>
+    </div>
+  );
+}
---
+++ src/services/firestore.ts
@@ -0,0 +1,80 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  Timestamp,
+  collection,
+  CollectionReference,
+  DocumentData,
+  doc,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  orderBy,
+  FirestoreError,
+  QuerySnapshot,
+  deleteDoc,
+  updateDoc,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firestore = getFirestore(firebaseApp);
+
+// This is just a helper to add the type to the db responses
+const createCollection = <T = DocumentData>(collectionName: string) => {
+  return collection(firestore, collectionName) as CollectionReference<T>;
+};
+
+// Import all your model types
+export interface Thing {
+  created: Timestamp;
+  content: string;
+}
+
+export type ThingWithId = Thing & {
+  id: string;
+};
+
+// export all your collections
+export const thingsCol = createCollection<Thing>('things');
+
+export const getThings = () => {
+  return getDocs(thingsCol);
+};
+
+export const onThingsUpdate = (
+  onNext: (snapshot: QuerySnapshot<Thing>) => void,
+  onError?: (error: FirestoreError) => void,
+  onCompletion?: () => void,
+) => {
+  const thingsQueryOrderedByDate = query(thingsCol, orderBy('created'));
+  return onSnapshot(thingsQueryOrderedByDate, onNext, onError, onCompletion);
+};
+
+export const addThing = (thingText: string) => {
+  addDoc(thingsCol, {
+    content: thingText,
+    created: serverTimestamp(),
+  });
+};
+
+export const deleteThing = async (thingId: string) => {
+  const thingReference = doc(thingsCol, thingId);
+  await deleteDoc(thingReference);
+};
+
+const editThing = async (thingId: string, newThingContent: string) => {
+  const thingReference = doc(thingsCol, thingId);
+  await updateDoc(thingReference, { content: newThingContent });
+};
+
+export default {
+  editThing,
+};
